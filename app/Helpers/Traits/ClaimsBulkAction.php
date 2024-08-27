80f5c4d550dde32afa5b047137320899636afffa
Jai Kumar <89125772+Jaikumar0101@users.noreply.github.com>
2024-08-27 19:20:48 +0530

    Add files via upload

---
+++ app/Helpers/Traits/ClaimsBulkAction.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Helpers\Traits;
+
+use App\Models\InsuranceClaim;
+
+trait ClaimsBulkAction
+{
+    public function permanentlyRemoveRecords():void
+    {
+        if (count($this->selected)>0)
+        {
+            InsuranceClaim::withTrashed()->whereIn('id',$this->selected)->forceDelete();
+            $this->resetSelectedRows();
+            $this->dispatch(
+                "SetMessage",
+                type:'success',message:'Removed Successfully',
+            );
+        }
+        else
+        {
+            $this->dispatch(
+                "SetMessage",
+                type:'info',message:'Please select at least one record',
+            );
+        }
+    }
+
+    public function moveRecordsToTrashed():void
+    {
+        if (count($this->selected)>0)
+        {
+            InsuranceClaim::withTrashed()->whereIn('id',$this->selected)->delete();
+            $this->resetSelectedRows();
+            $this->dispatch(
+                "SetMessage",
+                type:'success',message:'Moved to Archived',
+            );
+        }
+        else
+        {
+            $this->dispatch(
+                "SetMessage",
+                type:'info',message:'Please select at least one record',
+            );
+        }
+    }
+
+    public function restoreRecords():void
+    {
+        if (count($this->selected)>0)
+        {
+            InsuranceClaim::onlyTrashed()->whereIn('id',$this->selected)->restore();
+            $this->resetSelectedRows();
+            $this->dispatch(
+                "SetMessage",
+                type:'success',message:'Records Restored',
+            );
+        }
+        else
+        {
+            $this->dispatch(
+                "SetMessage",
+                type:'info',message:'Please select at least one record',
+            );
+        }
+    }
+}
